5fda64179d6400d05097c4058143ec7e683e3960
Кухта Иван <kuhtaia@it2g.tu>
2019-06-02 09:47:41 +0700

    HW04 - webapp

---
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/WebAppServlet.java
@@ -0,0 +1,30 @@
+package ru.javaops.masterjava.webapp;
+
+import org.thymeleaf.context.WebContext;
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.persist.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
+
+@WebServlet(urlPatterns = "/", loadOnStartup = 1)
+public class WebAppServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
+
+        UserDao userDao = DBIProvider.getDao(UserDao.class);
+        List<User> users = userDao.getWithLimit(20);
+
+        webContext.setVariable("users", users);
+        engine.process("result", webContext, resp.getWriter());
+    }
+}
